91830d4327c1038e4b87e264fe6fe0793d613dfd
unknown <kaanaksoyaz@gmail.com>
2014-04-18 11:30:58 -0700

    Second Commit

--- test.go
+++ test.go
@@ -3,34 +3,46 @@ package main
 import (
 	"html/template"
 	"io/ioutil"
+	"log"
 	"net/http"
 )
 
-type Page struct {
-	Body []byte
+type Problem struct {
+	Question []byte
+	Answer   []byte
 }
 
 var filename string = "data.txt"
 
-func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Problem) {
 	t, _ := template.ParseFiles(tmpl)
 	t.Execute(w, p)
 }
 
-func (p *Page) save() error {
-	return ioutil.WriteFile(filename, p.Body, 0600)
+func (p *Problem) save() error {
+	return ioutil.WriteFile(filename, p.Question, 0600)
 }
 
-func loadPage(title string) *Page {
+func loadProblem(title string) *Problem {
 	body, _ := ioutil.ReadFile(filename)
-	return &Page{Body: body}
+	return &Problem{Question: body}
 }
 func handler(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "home.html", &Page{Body: []byte("Get Em All in a line")})
+	renderTemplate(w, "home.html", &Problem{Question: []byte("Test")})
 
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request) {
+	question := r.FormValue("question")
+	log.Println("Text: ", question)
+
+	//	p := &Problem{Question: question, Answer: []byte("")}
+	//	p.save()
+	//	http.Redirect(w, r, "/troll", http.StatusFound)
 }
 
 func main() {
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/save/", saveHandler)
 	http.ListenAndServe(":8080", nil)
 }
